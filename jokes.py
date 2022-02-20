984447b35abb66c656aa114db877055761377229
Mickey Huang <mickey19970109@gmail.com>
2022-02-20 14:50:38 +0800

    Version 1

--- jokes.py
+++ jokes.py
@@ -3,6 +3,23 @@
 # Packages
 import requests
 import json
+import random
 
+url = "https://icanhazdadjoke.com/search"
+search = input("Search:")
+response = requests.get(url,headers={"Accept":"application/json"},params={"term":f"{search}"})
+response_text = json.loads(response.text)
+results = response_text["results"]
+total_jokes = response_text["total_jokes"]
 
-
+if results != []:
+    joke = random.choice(results)["joke"]
+    print(f"Total jokes:{total_jokes}")
+    print(f"One of them:\n{joke}")
+    with open("jokes.txt","a") as txt:
+        txt.write(f"Total jokes:{total_jokes}\n")
+        txt.write(f"One of them:\n{joke}\n")
+else:
+    print("No joke here")
+    with open("jokes.txt","a") as txt:
+        txt.write(f"Search[{search}]:No joke here\n")
